8720f2890470bdf449305653af34c5038ea17133
tumfwkm <tumf@wakumo.vn>
2024-04-21 09:27:33 +0700

    - accept MONGODUMP_OPTS - dump execution is surronded by try block - print() to log()

--- backup.py
+++ backup.py
@@ -14,6 +14,11 @@ BACKUP_FILE_PREFIX = os.getenv("BACKUP_FILE_PREFIX", "backup")
 GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
 GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 GCP_SERVICE_ACCOUNT_KEY_BASE64 = os.getenv("GCP_SERVICE_ACCOUNT_KEY_BASE64")
+MONGODUMP_OPTS = os.getenv("MONGODUMP_OPTS", "")
+
+
+def log(message):
+    print(f"{datetime.now()}: {message}", flush=True)
 
 
 def backup_mongodb_to_gcs():
@@ -23,10 +28,18 @@ def backup_mongodb_to_gcs():
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
         backup_filename = f"{BACKUP_FILE_PREFIX}-{timestamp}.gz"
         # Run the mongodump command to backup and compress using gzip
-        dump_cmd = f"mongodump --host={MONGODB_HOST} --archive={backup_filepath} --gzip --quiet"
-        print(f"Execute: {dump_cmd}")
-        subprocess.run(dump_cmd, shell=True, check=True)
-        upload_to_gcs(backup_filepath, backup_filename)
+        dump_cmd = f"mongodump --host={MONGODB_HOST} --archive={backup_filepath} --gzip {MONGODUMP_OPTS}"
+        log(f"Execute({datetime.now()}): {dump_cmd}")
+        try:
+            subprocess.run(dump_cmd, shell=True, check=True)
+            file_size = os.path.getsize(backup_filepath)
+            log(
+                f"...done({datetime.now()}), file size: {file_size / (1024 * 1024):.2f} MB"
+            )
+            upload_to_gcs(backup_filepath, backup_filename)
+        except subprocess.CalledProcessError as e:
+            log(f"...error({datetime.now()}): {e}")
+            return
 
 
 def get_storage_client():
@@ -46,16 +59,16 @@ def list_bucket():
     bucket = storage_client.bucket(GCS_BUCKET_NAME)
     blobs = bucket.list_blobs()
     for blob in blobs:
-        print(blob.name)
+        log(blob.name)
 
 
 def prune_bucket(days=7, minimum_backups=7):
-    print(f"Prune bucket days:{days} minimum_backups:{minimum_backups}")
+    log(f"Prune bucket days:{days} minimum_backups:{minimum_backups}")
     storage_client = get_storage_client()
     bucket = storage_client.bucket(GCS_BUCKET_NAME)
     blobs = list(bucket.list_blobs())
     if len(blobs) <= minimum_backups:
-        print(f"Do not prune if not enough backups {len(blobs)}/{minimum_backups}")
+        log(f"Do not prune if not enough backups {len(blobs)}/{minimum_backups}")
         return
     cutoff_date = datetime.now(timezone.utc) - timedelta(
         days=days
@@ -63,18 +76,18 @@ def prune_bucket(days=7, minimum_backups=7):
     for blob in blobs:
         if blob.time_created < cutoff_date:
             blob.delete()
-            print(f"Deleted {blob.name}")
+            log(f"Deleted {blob.name}")
 
 
 def upload_to_gcs(file_path, name):
-    print(f"Upload to GCS {file_path} {name}")
+    log(f"Upload to GCS {file_path} {name}")
     storage_client = get_storage_client()
     bucket = storage_client.bucket(GCS_BUCKET_NAME)
 
     blob = bucket.blob(name)
 
     blob.upload_from_filename(file_path, timeout=20 * 60)
-    print(f"A file {file_path} has been uploaded to {GCS_BUCKET_NAME}.")
+    log(f"A file {file_path} has been uploaded to {GCS_BUCKET_NAME}.")
 
 
 if __name__ == "__main__":
